0ee6a616de22f1429c0a39d8463b895c9749b19a
endersonlg <endersonluciog@gmail.com>
2020-02-18 02:39:02 -0300

    Projeto

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: '123',
+  database: 'gobaber',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoreAll: true,
+  },
+};
