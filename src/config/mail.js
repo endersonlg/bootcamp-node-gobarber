37e6b81971fdfab863cc7d194037b80882e19130
endersonlg <endersonluciog@gmail.com>
2020-02-20 21:09:34 -0300

    16 - Configurando Nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'bc06420b71ea23',
+    pass: 'ced3b2febb2842',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply>@gobarber.com',
+  },
+};
