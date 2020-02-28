46ec8d66768acf4a67324ac52fbd17e2dd0ea2b3
endersonlg <endersonluciog@gmail.com>
2020-02-28 03:07:49 -0300

    23 - Variáveis ambiente

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: 'bc06420b71ea23',
-    pass: 'ced3b2febb2842',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe GoBarber <noreply>@gobarber.com',
--- src/config/sentry.js
+++ src/config/sentry.js
@@ -1,3 +1,3 @@
 export default {
-  dsn: 'https://8c31cb0bfd1449ac8490c0db4629228d@sentry.io/3111108',
+  dsn: process.env.SENTRY_DSN,
 };
